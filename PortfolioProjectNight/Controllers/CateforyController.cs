437866bf8a69a6b079340a9faf9632510b377f32
turhanbey45@gmail.com <turhanbey45@gmail.com>
2024-09-10 21:51:53 +0300

    2. Ders Tamamlandi

---
+++ PortfolioProjectNight/Controllers/CateforyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class CateforyController : Controller
+    {
+       
+        public ActionResult CategoryList()
+        {
+            return View();
+        }
+
+        public ActionResult CreeateCategory()
+        {
+            return View();
+        }
+    }
+}
